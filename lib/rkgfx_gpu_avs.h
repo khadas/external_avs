e96bb220597fb8b733591d95db2b3c665d369ac1
maodong.zhou <kyrie.zhou@rock-chips.com>
2022-04-19 09:30:57 +0800

    release librkgfx_avs.so v4.1 1.change mesh_step from 8x8 to 32x32 2.change yuv colorspace from BT601_limit_range to BT601_full_range 3.support yuv_420_8bit afbc input and output
    Signed-off-by: maodong.zhou <kyrie.zhou@rock-chips.com>
    Change-Id: I9514678f5cfb9c879e5e7089cfd76106f28188fe

---
+++ lib/rkgfx_gpu_avs.h
@@ -0,0 +1,34 @@
+
+typedef unsigned int uint32_t;
+
+typedef struct RKGFX_AVS_context_s {
+    uint32_t apiVersion;
+    uint32_t numView;
+    char * meshFilePath;
+    uint32_t inputBufferWidth;
+    uint32_t inputBufferHeight;
+    uint32_t inputBufferFormat;
+    uint32_t inputIsAFBC;
+    uint32_t outputBufferWidth;
+    uint32_t outputBufferHeight;
+    uint32_t outputBufferFormat;
+    uint32_t outputIsAFBC;
+    uint32_t useFd;
+    int* inputFdArray;
+    uint32_t useSync;
+} RKGFX_AVS_context_t;
+
+typedef enum {
+RKGFX_AVS_SUCCESS,
+RKGFX_AVS_INVALID_PARAM,
+RKGFX_AVS_FAILED,
+} RKGFX_AVS_STATUS;
+
+extern "C" RKGFX_AVS_STATUS RKGFX_AVS_init (void* context);
+extern "C" RKGFX_AVS_STATUS RKGFX_AVS_AllProcess (void* context);
+extern "C" RKGFX_AVS_STATUS RKGFX_AVS_deinit (void* context);
+extern "C" void* RKGFX_AVS_create_sync(void);
+extern "C" RKGFX_AVS_STATUS RKGFX_AVS_wait_sync(void* gles_proceess_sync);
+
+
+
